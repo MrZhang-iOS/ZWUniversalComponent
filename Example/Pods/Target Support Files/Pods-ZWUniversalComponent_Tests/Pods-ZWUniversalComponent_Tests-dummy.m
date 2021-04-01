8010841c84ed2a4f56648a72fb114ee3c40ef584
MrZhang-iOS <why.20130509@gmail.com>
2021-04-01 18:39:05 +0800

    终端创建通用组件项目 pod lib create ZWUniversalComponent

---
+++ Example/Pods/Target Support Files/Pods-ZWUniversalComponent_Tests/Pods-ZWUniversalComponent_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ZWUniversalComponent_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ZWUniversalComponent_Tests
+@end
---
+++ Example/Pods/Target Support Files/ZWUniversalComponent/ZWUniversalComponent-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ZWUniversalComponent : NSObject
+@end
+@implementation PodsDummy_ZWUniversalComponent
+@end
